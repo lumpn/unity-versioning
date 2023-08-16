48c66a301dee85e9e7eba06e8ff54db18a21dfe1
lumpn <lumpn@users.noreply.github.com>
2023-08-16 22:45:30 +0800

    Context menu

--- Editor/AssetUtils.cs
+++ Editor/AssetUtils.cs
@@ -12,12 +12,17 @@ namespace Lumpn.Versioning
     public static class AssetUtils
     {
         [MenuItem("Utils/Versioning/Mark dirty (selected files)")]
-        public static void MarkDirty()
+        [MenuItem("Assets/Mark dirty")]
+        [MenuItem("CONTEXT/Transform/Mark dirty")]
+        [MenuItem("CONTEXT/GameObject/Mark dirty")]
+        [MenuItem("CONTEXT/Object/Mark dirty")]
+        public static void MarkDirty(MenuCommand cmd)
         {
+            var context = cmd.context;
             var guids = Selection.assetGUIDs;
-            Debug.Log(string.Join(", ", guids));
+            Debug.Log(string.Join(", ", guids), context);
 
-            foreach(var guid in guids)
+            foreach (var guid in guids)
             {
                 var path = AssetDatabase.GUIDToAssetPath(guid);
                 var obj = AssetDatabase.LoadAssetAtPath<Object>(path);
